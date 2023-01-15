52bf2d97190cbe15993e8815859dc486a50a883d
Joao Lopes <jandrevl@gmail.com>
2023-01-15 18:56:35 +0000

    Angular course project. First commit.

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,13 @@
+import { Recipe } from './../recipe.model';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes: Recipe[] =[
+    new Recipe('A Test Recipe', 'This is Just a Test', 'https://thebrilliantkitchen.com/wp-content/uploads/2022/08/Lamb-Medium-Rare%E2%80%8B%E2%80%8B.jpg'), new Recipe('A Test Recipe', 'This is Just a Test', 'https://thebrilliantkitchen.com/wp-content/uploads/2022/08/Lamb-Medium-Rare%E2%80%8B%E2%80%8B.jpg')
+  ];
+}
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,16 @@
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+
+    // constructor (name: string, amount: number) {
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+
+    // other way of doing this:
+
+    constructor(public name: string, public amount: number) {
+
+    }
+
+}
