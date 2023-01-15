89ea41dd1a39c7228bf36d844bb9691888d636d9
Joao Lopes <jandrevl@gmail.com>
2023-01-15 22:03:22 +0000

    Add the 'Add ingredients to the shopping list' feature

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Recipe } from './../recipe.model';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,9 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() recipeChosen = new EventEmitter<Recipe>()
   
   recipes: Recipe[] =[
     new Recipe('A Test Recipe', 'This is Just a Test', 'https://thebrilliantkitchen.com/wp-content/uploads/2022/08/Lamb-Medium-Rare%E2%80%8B%E2%80%8B.jpg'),
-    new Recipe('A Test Recipe', 'This is Just a Test', 'https://thebrilliantkitchen.com/wp-content/uploads/2022/08/Lamb-Medium-Rare%E2%80%8B%E2%80%8B.jpg')
+    new Recipe('Another Test Recipe', 'This is Just another Test for the recipe APP', 'https://thebrilliantkitchen.com/wp-content/uploads/2022/08/Lamb-Medium-Rare%E2%80%8B%E2%80%8B.jpg')
   ];
+
+  onSelectRecipe(recipe: Recipe) {
+    this.recipeChosen.emit(recipe);
+  }
+
 }
